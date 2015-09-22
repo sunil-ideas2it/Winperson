395529b59515486211e6927bb30b9e754a0a0e6b
Sunil <sunilhirole@ideas2it.com>
2015-09-22 17:44:38 +0530

    Task: Configured grunt task

---
+++ tasks/config/jshint.js
@@ -0,0 +1,24 @@
+/**
+ * Clean files and folders.
+ *
+ * ---------------------------------------------------------------
+ *
+ * This grunt task is configured to clean out the contents in the .tmp/public of your
+ * sails project.
+ *
+ * For usage docs see:
+ * 		https://github.com/gruntjs/grunt-contrib-clean
+ */
+module.exports = function(grunt) {
+
+	grunt.config.set('jshint', {
+		dev: [
+		'api/controllers/*.js',
+		'api/models/*.js',
+		'assets/js/controller/*.js',
+		'assets/js/app.js',
+		],
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+};
