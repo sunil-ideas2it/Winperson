1f60e255a772725aa040ba94a2222e99c070df01
Sunil <sunilhirole@ideas2it.com>
2015-09-23 19:18:45 +0530

    Bug: AnswerController grunt errors fixed

--- api/controllers/AnswerController.js
+++ api/controllers/AnswerController.js
@@ -1,43 +1,33 @@
 module.exports = {
 
-    /**
-     * Check the provided email address and password, and if they
-     * match a real user in the database, sign in to Activity Overlord.
-     */
-    getJob: function(req, res) {
-        // Try to look up user using the provided email address
-        Job.find({}).exec(function findCB(err, found) {
+  // Check the provided email address and password, and if they
+  // match a real user in the database, sign in to Winperson.
+  //
+  getJob: function(req, res) {
+    // Try to look up user using the provided email address
+    Job.find({}).exec(function findCB(err, found) {
+      res.json({
+        jobObject: found,
+      });
+    });
+  },
 
-            res.json({
-                jobObject: found
-            });
-
-        });
-
-    },
-
-    getAnswer: function(req, res) {
-        // Try to look up user using the provided email address
-        Answer.find({
-            jobId: req.body.id
-        }).exec(function findCB(err, found) {
-            res.json({
-                answerObject: found
-            });
-
-        });
-
-    },
-     getUserDetail: function(req, res) {
-        // Try to look up user using the provided email address
-        Answer.find({
-            videoFileName: req.body.videoFileName
-        }).exec(function findCB(err, found) {
-            res.json(found);
-
-        });
-
-    }
-
-
+  getAnswer: function(req, res) {
+    // Try to look up user using the provided email address
+    Answer.find({
+      jobId: req.body.id,
+    }).exec(function findCB(err, found) {
+      res.json({
+        answerObject: found,
+      });
+    });
+  },
+  getUserDetail: function(req, res) {
+    // Try to look up user using the provided email address
+    Answer.find({
+      videoFileName: req.body.videoFileName,
+    }).exec(function findCB(err, found) {
+      res.json(found);
+    });
+  },
 };
